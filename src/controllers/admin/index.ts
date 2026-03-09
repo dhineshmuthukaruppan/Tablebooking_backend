5b8ef3e76325268f7dbdb296000f47d1d78dfc39
ganesh m <ganeshmuthukaruppan@gmail.com>
2026-03-09 17:44:29 +0530

    milestone 1 finished

--- src/controllers/admin/index.ts
+++ src/controllers/admin/index.ts
@@ -1,2 +1,3 @@
 export { dashboardHandler } from "./dashboard.handler";
 export { getUsersHandler, patchUserHandler } from "./users.handler";
+export { patchBookingByAdminHandler } from "./bookings.handler";
